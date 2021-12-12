a58686b284f9b3deed3ef93d9601792fac0763fc
Artem <malpolon76@gmail.com>
2021-12-12 21:24:47 +0200

    release AuthGuard

--- src/app/routers/app-routers.module.ts
+++ src/app/routers/app-routers.module.ts
@@ -10,12 +10,13 @@ import { ShoppingListComponent } from '../shopping-list/shopping-list.component'
 import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
 
 import { RecipesResolverServise } from '../recipes/recipes-resolver.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
+  {path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
     {path: '', component: RecipesStartComponent },
     {path: 'new', component: RecipeEditComponent},
     {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverServise]},

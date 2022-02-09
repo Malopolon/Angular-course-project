c3b2f599ff7f91524e0b26d8a30bf35d8878bb47
Artem Vlasenko <malpolon76@gmail.com>
2022-02-09 19:30:35 +0200

    add recipe and shopping list modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutersModule } from './routers/app-routers.module';
-import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
-    RecipesStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent
   ],
@@ -42,7 +28,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutersModule
+    AppRoutersModule,
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesResolverServise } from "./recipes-resolver.service";
+import { RecipesStartComponent } from "./recipes-start/recipes-start.component";
+import { RecipesComponent } from "./recipes.component";
+
+const routes: Routes = [
+    {
+        path: 'recipes', component: RecipesComponent, 
+            children: [
+                    {path: '', component: RecipesStartComponent },
+                    {path: 'new', component: RecipeEditComponent},
+                    {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverServise]},
+                    {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverServise]}
+            ]
+    }
+]    
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+
+export class RecipesRoutingModule {}

fca846fa46d6bcc885d59d74152394a32345ad67
Artem Vlasenko <malpolon76@gmail.com>
2023-01-16 22:52:36 +0200

    cli update

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -3,10 +3,10 @@ import { switchMap } from 'rxjs/operators'
 import * as AuthActions from './auth.actions'
 export class AuthEffects {
 
-    authLogin = this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START),
-        switchMap((authData: AuthActions.LoginStart))
-    )
+    // authLogin = this.actions$.pipe(
+    //     ofType(AuthActions.LOGIN_START),
+    //     switchMap((authData: AuthActions.LoginStart))
+    // )
     constructor(private actions$: Actions) { }
 
 }
